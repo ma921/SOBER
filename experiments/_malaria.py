d33ceeffb2aef713a2d3a268858b613ef8b5a4c4
Masaki Adachi <61237927+ma921@users.noreply.github.com>
2023-07-03 17:50:31 +0900

    Add files via upload

--- experiments/_malaria.py
+++ experiments/_malaria.py
@@ -7,22 +7,22 @@ from SOBER._prior import DatasetPrior
 from experiments._generate_drug_dataset import create_malaria_dataset
 
 
-def featurise_dataset():
-    MALARIA_DIR_NAME = "../experiments/dataset/"
+def featurise_dataset(data_path):
+    #MALARIA_DIR_NAME = "../experiments/dataset/"
     # dataset can be downloaded in  https://www.mmv.org/mmv-open/malaria-box/malaria-box-supporting-information
-    data_filename = "malaria_box_dataset.csv"
-    data_path = os.path.join(MALARIA_DIR_NAME, data_filename)
+    #data_filename = "malaria_box_dataset.csv"
+    #data_path = os.path.join(MALARIA_DIR_NAME, data_filename)
     features, true_targets = create_malaria_dataset(data_path)
     return features, true_targets
     
-def setup_malaria():
+def setup_malaria(data_path):
     """
     Set up the experiments with anti-malarial drug discovery task
     
     Return:
     - prior: class, the function of binary prior
     """
-    features, true_targets = featurise_dataset()
+    features, true_targets = featurise_dataset(data_path)
     prior = DatasetPrior(features, true_targets)
     
     return prior
--- experiments/_solvent.py
+++ experiments/_solvent.py
@@ -6,19 +6,23 @@ from abc import abstractmethod
 from SOBER._prior import DatasetPrior
 from experiments._generate_drug_dataset import create_solvent_dataset
 
-    
-def setup_solvent():
+
+def featurise_dataset(data_path):
+    #SOLVENT_DIR_NAME = "./experiments/dataset/"
+    # dataset can be downloaded in http://quantum-machine.org/datasets/ 
+    #data_filename = "QM9_dipole.csv"
+    #data_path = os.path.join(SOLVENT_DIR_NAME, data_filename)
+    features, true_targets = create_solvent_dataset(data_path)
+    return features, true_targets
+
+def setup_solvent(data_path):
     """
     Set up the experiments with solvent materials discovery task
     
     Return:
     - prior: class, the function of dataset prior
     """
-    SOLVENT_DIR_NAME = "./experiments/dataset/"
-    # dataset can be downloaded in http://quantum-machine.org/datasets/ 
-    data_filename = "QM9_dipole.csv"
-    data_path = os.path.join(SOLVENT_DIR_NAME, data_filename)
-    features, true_targets = create_solvent_dataset(data_path)
+    features, true_targets = featurise_dataset(data_path)
     prior = DatasetPrior(features, true_targets)
     
     return prior
