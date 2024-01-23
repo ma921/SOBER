f785cef2922a9862f32f632bc681d963a42bf0e3
Yannick Kuhn <yannick.kuhn@dlr.de>
2024-01-23 14:56:12 +0100

    made settings configurable from scripts

--- SOBER/_settings.py
+++ SOBER/_settings.py
@@ -1,12 +1,22 @@
 import torch
 
+_device = (
+    torch.device('cuda')
+    if torch.cuda.is_available()
+    else torch.device('cpu')
+)
+_dtype = torch.double
 
-def setting_parameters():
+
+def setting_parameters(device=None, dtype=None):
     """
     Return:
        - device: torch.device, cpu or cuda
        - dtype: torch.dtype, torch.float or torch.double
     """
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    dtype = torch.double
-    return device, dtype
+    global _device, _dtype
+    if device:
+        _device = device
+    if dtype:
+        _dtype = dtype
+    return _device, _dtype
