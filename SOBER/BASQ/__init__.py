1478e82c301e471bdfcf956dcf00416bd83d7673
Yannick Kuhn <yannick.kuhn@dlr.de>
2025-01-24 16:05:43 +0100

    added simplified interface and extension capabilities to SOBER for ease-of-use and reuseability

---
+++ SOBER/BASQ/__init__.py
@@ -0,0 +1,4 @@
+from ._basq import BASQ
+from ._scale_mmlt import ScaleMmltGP
+
+__all__ = ['BASQ', 'ScaleMmltGP']
--- SOBER/__init__.py
+++ SOBER/__init__.py
@@ -1,3 +1,6 @@
 from ._settings import setting_parameters
+from ._sober import Sober
+from ._sober_wrapper import SoberWrapper
+from ._utils import TensorManager
 
-__all__ = ['setting_parameters',]
+__all__ = ['setting_parameters', 'Sober', 'SoberWrapper', 'TensorManager']
