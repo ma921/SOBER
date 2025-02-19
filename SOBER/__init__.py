5904cc53f457557e508fe12fa45292b559916156
Yannick Kuhn <yannick.kuhn@dlr.de>
2025-02-19 10:55:17 +0100

    Expectation Propagation and Inverse Modelling considered stable and added to __init__

--- SOBER/__init__.py
+++ SOBER/__init__.py
@@ -1,6 +1,15 @@
+from ._expectation_propagation import ExpectationPropagation
+from ._inverse_modelling import InverseModel
 from ._settings import setting_parameters
 from ._sober import Sober
 from ._sober_wrapper import SoberWrapper
 from ._utils import TensorManager
 
-__all__ = ['setting_parameters', 'Sober', 'SoberWrapper', 'TensorManager']
+__all__ = [
+    'ExpectationPropagation',
+    'InverseModel',
+    'setting_parameters',
+    'Sober',
+    'SoberWrapper',
+    'TensorManager'
+]
